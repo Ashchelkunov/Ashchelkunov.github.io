1a4fe98c98f8976173ca26bf0a7643dd1de9e114
Ashchelkunov <nevermind0996@yandex.ru>
2019-11-26 00:28:26 +0300

    photo

--- src/main.js
+++ src/main.js
@@ -1,27 +1,11 @@
-// import $ from "jquery"
+import $ from "jquery"
 
-import "bxslider/dist/jquery.bxslider"
 $(document).ready(function(){
-  $('.slider').bxSlider({
-  	minSlides: 1,
-		maxSlides: 4,
-    slideWidth: 258,
-    slideMargin: 32,
-    moveSlides: 1,
-    pager: false,
-    nextText: '',
-    prevText: '',
-    infiniteLoop: false,
-    responsive: true,
-  });
+    $('.contatcs-btn').on('click', function(event){
+      event.preventDefault();
+      $('.contatcs').fadeIn();
+    });
+    $('.contatcs-close').on('click', function(event){
+      $('.contatcs').fadeOut();
+    })
 });
-import "bxslider/dist/jquery.bxslider"
-$(document).ready(function(){
-  $('.slider-review').bxSlider({
-    maxSlides: 1,
-    nextText: '',
-    prevText: '',
-    pager: true,
-    controls: false,
-  })
-})
